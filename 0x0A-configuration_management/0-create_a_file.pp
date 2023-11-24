cdf04cbe4db65829697616fccf7c3051ce0130f3
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-11-24 15:34:35 +0000

    first puppe file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# creates a file, set permissions and owners and adds content
+file { '/tmp/school':
+   ensure  => file,
+   owner   => www-data,
+   group   => www-data,
+   mode    => '0744',
+   content => 'I love puppet'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+intro into puppet configuration mangement tool
